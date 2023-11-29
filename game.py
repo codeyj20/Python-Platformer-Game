6b8500fb2eb426e38437a6e03d204551c68a0f5e
Jamee Cody <jamee3059@yahoo.com>
2023-11-29 17:31:51 -0600

    Create the background

--- game.py
+++ game.py
@@ -8,15 +8,35 @@ pygame.init()
 
 pygame.display.set_caption("Platform Game")
 # Global variables
-BG_COLOR = (255, 255, 255)
 WIDTH, HEIGHT = 1000, 700
 FPS = 60
 PLAYER_VEL = 5 # Player's speed
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
+
+def get_background(name):
+    image = pygame.image.load(join("assets", "Background", name))
+    _, _, width, height = image.get_rect()
+    tiles = []
+
+    for i in range(WIDTH // width + 1):
+        for j in range (HEIGHT // height + 1):
+            pos = (i * width, j * height)
+            tiles.append(pos)
+
+    return tiles, image
+
+def draw(window, background, bg_image):
+    for tile in background:
+        window.blit(bg_image, tile)
+
+    pygame.display.update()
+
+
 # Main function
 def main(window):
     clock = pygame.time.Clock()
+    background, bg_image = get_background("Purple.png")
 
     run = True
     while run:
@@ -26,6 +46,9 @@ def main(window):
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+        draw(window, background, bg_image)   
+         
     pygame.quit()
     quit()
 
